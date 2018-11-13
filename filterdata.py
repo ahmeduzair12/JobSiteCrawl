e6b080afd3d549c6acb94c982c7dcde04457d9ba
Rohit Singh <rohit@cloudinfra.in>
2018-11-13 22:40:37 +0530

    Monster renamed, filtered data for both to csv, added naukri

--- filterdata.py
+++ filterdata.py
@@ -1,11 +1,15 @@
 import json, csv
 
 
-f_csv = open('quotes.csv', 'w', encoding='utf-8')
+f_csv = open('jobs.csv', 'w', encoding='utf-8')
 f_json = open('quotes.json', 'r', encoding='utf-8')
-
+n_json = open('naukri.json', 'r', encoding='utf-8')
 json_data = json.load(f_json)
 csv_data = []
+for i in json_data:
+    csv_data.append([i['company'], i['summary'], i['source'], i['link'], i['date'], i['position']])
+json_data = json.load(n_json)
+
 for i in json_data:
     csv_data.append([i['company'], i['summary'], i['source'], i['link'], i['date'], i['position']])
 
